16cbe77408fd688f049deb53d9a3a1a19c08bb3e
Anushree <anushreebhatt357@gmail.com>
2025-07-06 15:32:58 -0400

    variables, types, assignments, constant, explicit type conversion

--- app.go
+++ app.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Hello World! Welcome to GoLang ^ _ ^")
+	fmt.Println("Hello World! Welcome to GoLang Future value prediction for invested amount^ _ ^")
+	futureValue()
+}
+
+func futureValue() {
+	const inflationRate = 6.0
+	principle := 1000
+	rate_of_interest := 5.5
+	var years float64 = 10 // explicit type assignment
+
+	futureValue := float64(principle) * math.Pow(1+rate_of_interest/100, years)
+	realFutureValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println("Expected returns:", futureValue)
+	fmt.Println("Actual returns:", realFutureValue)
+
 }
