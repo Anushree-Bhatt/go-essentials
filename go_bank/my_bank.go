f6b2de2974886b0bc5789fc3f2a2fc41f7a84358
Anushree <anushreebhatt357@gmail.com>
2025-07-07 07:32:31 -0400

    Coded atm logic for Go-bank using switch statement

--- go_bank/my_bank.go
+++ go_bank/my_bank.go
@@ -4,9 +4,50 @@ import (
 	"fmt"
 )
 
-func bank_using_if() {
-	balance := 1000.0
+func bank_using_switch(balance float64) {
+	for {
+		ch := display_start()
 
+		switch ch {
+		case 1:
+			fmt.Println("Your balance:", balance)
+		case 2:
+			var to_deposit float64
+			fmt.Print("Enter the amount to deposit:")
+			fmt.Scan(&to_deposit)
+
+			if to_deposit <= 0 {
+				fmt.Print("Invalid amount. You cannot deposit this amount")
+				continue
+			}
+
+			balance = balance + to_deposit
+			fmt.Println("Your amount is deposited. New balance:", balance)
+		case 3:
+			var to_withdraw float64
+			fmt.Print("Enter the amount to withdraw:")
+			fmt.Scan(&to_withdraw)
+
+			if to_withdraw <= 0 {
+				fmt.Println("Invalid amount. You cannot withdraw this amount")
+				continue
+			}
+			if to_withdraw > balance {
+				fmt.Println("Your withdrawal amount exceeds current balance!")
+				continue
+			}
+
+			balance = balance - to_withdraw
+			fmt.Println("You've withdrawn the amount. New balance:", balance)
+		default:
+			return
+
+		}
+
+	}
+}
+
+func bank_using_if(balance float64) {
 	for {
 		ch := display_start()
 
@@ -41,10 +82,9 @@ func bank_using_if() {
 			balance = balance - to_withdraw
 			fmt.Println("You've withdrawn the amount. New balance:", balance)
 		} else {
-			break
+			return
 		}
 	}
-	fmt.Println("Thank you for visiting!")
 }
 
 func display_start() int {
@@ -64,5 +104,7 @@ func display_start() int {
 
 func main() {
 	fmt.Println("Welcome to Go Bank!")
-	bank_using_if()
+	// bank_using_if(1000)
+	bank_using_switch(1000)
+	fmt.Println("Thank you for visiting!")
 }
