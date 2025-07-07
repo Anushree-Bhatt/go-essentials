0ecc88d1fecfb1d9fe10f5706cb11399daabf4ae
Anushree <anushreebhatt357@gmail.com>
2025-07-06 20:29:01 -0400

    Did practice on some String formattings

--- go_essentials/app.go
+++ go_essentials/app.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World! Welcome to GoLang Future value prediction for invested amount^ _ ^")
-	futureValue()
+	fmt.Println("Hello World! Welcome to GoLang")
+	// futureValue()
+	stringFormatting()
 }
 
 func futureValue() {
+	//variables and types
 	const inflationRate = 6.0
 	var principle float64
 	var rate_of_interest float64
@@ -30,5 +32,32 @@ func futureValue() {
 
 	fmt.Println("Expected returns:", futureValue)
 	fmt.Println("Actual returns:", realFutureValue)
-	
+
+}
+
+func stringFormatting() {
+	fmt.Print("Hello Hi Namaskara!\n")
+
+	fmt.Println("Hello Hi")
+	fmt.Println("Namaskara")
+
+	fmt.Printf("%v %v %v\n", "Hello", "hi", "Namaskara\n")
+
+	text := `Using specifiers %.0f and %.2f`
+	fmt.Print(text)
+	a := 10.6
+	b := 20.43
+	fmt.Printf("Numbers are %.0f and %.2f\n", a, b)
+
+	fmt.Println("String concatenation using +")
+	s1 := "Hey!"
+	s2 := "How're you doing?"
+	fmt.Printf("%v\n", (s1 + s2))
+
+	//Multiple lines String using backticks - ` `
+	fmt.Print(`Using Backticks.....
+	...........
+	..........
+	Multiple Lines String!`)
+
 }
